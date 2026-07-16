4c0da7ea61b78e9d69232852daa5a10e0c149b74
Stepan <115882704+St3pAn1ndzy@users.noreply.github.com>
2026-07-16 23:22:14 +0400

    Update routes.py

--- routes.py
+++ routes.py
@@ -1,6 +1,7 @@
 from contextlib import asynccontextmanager
-from fastapi import FastAPI, HTTPException, Depends
-from sqlalchemy import desc, asc
+
+from fastapi import Depends, FastAPI, HTTPException
+from sqlalchemy import asc, desc
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.future import select
 
@@ -20,10 +21,9 @@ async def lifespan(app: FastAPI):
 app = FastAPI(lifespan=lifespan)
 
 
-@app.post('/recipes', response_model=schemas.RecipeOut)
+@app.post("/recipes", response_model=schemas.RecipeOut)
 async def create_recipe(
-        recipe: schemas.RecipeIn,
-        session: AsyncSession = Depends(get_session)
+    recipe: schemas.RecipeIn, session: AsyncSession = Depends(get_session)
 ):
     try:
         new_recipe = models.RecipeOrm(**recipe.model_dump())
@@ -37,28 +37,21 @@ async def create_recipe(
         raise HTTPException(status_code=400, detail=str(e))
 
 
-@app.get('/recipes', response_model=list[schemas.RecipeSummaryOut])
-async def get_recipes(
-        session: AsyncSession = Depends(get_session)
-):
+@app.get("/recipes", response_model=list[schemas.RecipeSummaryOut])
+async def get_recipes(session: AsyncSession = Depends(get_session)):
     result = await session.execute(
-        select(models.RecipeOrm)
-        .order_by(
-            desc(models.RecipeOrm.views),
-            asc(models.RecipeOrm.cook_time)
+        select(models.RecipeOrm).order_by(
+            desc(models.RecipeOrm.views), asc(models.RecipeOrm.cook_time)
         )
     )
     return result.scalars().all()
 
 
-@app.get('/recipes/{id_r}', response_model=schemas.RecipeOut)
-async def get_recipe(
-        id_r: int,
-        session: AsyncSession = Depends(get_session)
-):
+@app.get("/recipes/{id_r}", response_model=schemas.RecipeOut)
+async def get_recipe(id_r: int, session: AsyncSession = Depends(get_session)):
     recipe = await session.get(models.RecipeOrm, id_r)
     if not recipe:
-        raise HTTPException(status_code=404, detail='Recipe not found')
+        raise HTTPException(status_code=404, detail="Recipe not found")
 
     recipe.views += 1
     await session.commit()
